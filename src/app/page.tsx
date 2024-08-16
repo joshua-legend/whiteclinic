da6dcaac4e3382197d664f07e1ad282423846165
Zerokyh <koreaguh@naver.com>
2024-08-16 10:15:31 +0900

    ui update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col p-6">HOME 화면입니다</main>
   );
 }

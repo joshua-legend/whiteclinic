8280e94e9927dbe670b93c7d9c86b851af56f588
MyDesktop <dugubi1004@gmail.com>
2024-08-17 16:58:07 +0900

    0817 이지훈 - placeholder 변경, css Style 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,47 @@ export default function Home() {
     // 홈화면입니다.
     <main className="flex min-h-screen flex-col p-6">
       HOME 화면입니다
-      <MultiPurposeButton submitFn={() => console.log('Submit!')} text="등록" />
-      <MultiPurposeButton RegisterFn={() => console.log('Register!')} text="취소" />
-      <MultiPurposeButton ExRegisterFn={() => console.log('Extra Register!')} text="추가등록" />
-      <MultiPurposeButton salaryCheckFn={() => console.log('Check Salary!')} text="급여사항 확인" />
+      <div className="flex flex-col w-full h-fit">
+        <div className="flex flex-row w-full h-fit">
+          <MultiPurposeButton submitFn={() => console.log('Cancle Submit!')} text="아니오" />
+          <MultiPurposeButton submitFn={() => console.log('Submit!')} text="등록" />
+        </div>
+        <div>
+          <MultiPurposeButton RegisterFn={() => console.log('Cancle Register!')} text="취소" />
+          <MultiPurposeButton RegisterFn={() => console.log('Register!')} text="등록" />
+        </div>
+        <div className="flex flex-row w-full h-fit">
+          <MultiPurposeButton
+            ExRegisterFn={() => console.log('Cancle Extra Register!')}
+            text="등록중지"
+          />
+          <MultiPurposeButton ExRegisterFn={() => console.log('Extra Register!')} text="추가등록" />
+        </div>
+        <div className="flex flex-row w-full h-fit">
+          <MultiPurposeButton
+            salaryCheckFn={() => console.log('Check Salary!')}
+            text="급여사항 확인"
+          />
+        </div>
+      </div>
       <div>
-        <h1>Customer Info</h1>
-        <MultiPurposeInput name="John Doe" contact="123-456-7890" address="123 Main St" />
+        <h1>고객 정보</h1>
+        <MultiPurposeInput name="고객 성함" contact="고객 연락처" address="의뢰지" />
 
-        <h1>Special Notes</h1>
-        <MultiPurposeInput ps="This is a special note." />
+        <h1>특이 사항</h1>
+        <MultiPurposeInput ps="특이사항을 적어주세요." />
 
-        <h1>Discount Rate</h1>
+        <h1>할인율</h1>
         <MultiPurposeInput discount={15} />
 
-        <h1>Year Selection</h1>
+        <h1>연도입력</h1>
         <MultiPurposeInput thisYear={2024} />
 
-        <h1>Model Count</h1>
+        <h1>세척 대수</h1>
         <MultiPurposeInput modelEA={10} />
 
-        <h1>Calculate Something</h1>
-        <MultiPurposeInput CalFn={() => alert('Calculation done!')} />
+        <h1>계산된 값</h1>
+        <MultiPurposeInput CalFn={() => console.log('CalFn Called')} />
       </div>
     </main>
   );

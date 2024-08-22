a30f6a8a3d9dd7c46d6ddfc73c5ec2fe825e03d4
misnawk <kms13011@naver.com>
2024-08-22 10:41:13 +0900

    0822 강민석 - 체크박스 컴포넌트 이름변경, 카드, 체크박스 input값 ts로 구분해서 넣음

---
+++ src/app/atom/card/card.ts
@@ -0,0 +1,48 @@
+//기사별 스킬에따라 분류
+export type WallMounted = {
+  name: '김철수' | '이영희' | '박민수'; // 벽걸이
+};
+
+export type OneWay = {
+  name: '최지우' | '김하나' | '이준호'; // 원웨이
+};
+
+export type FourWay = {
+  name: '장민호' | '유미래' | '송중기'; // 포웨이
+};
+
+export type Circular = {
+  name: '박세리' | '윤호준' | '김혜수'; // 원형
+};
+
+export type Stand = {
+  name: '이수민' | '오지혜' | '강동원'; // 스탠드
+};
+
+export type OutdoorUnit = {
+  name: '한가람' | '최서현' | '류지석'; // 실외기
+};
+
+export type Duct = {
+  name: '백은정' | '신승민' | '조현우'; // 덕트
+};
+
+export type WindowType = {
+  name: '전지현' | '김우빈' | '한예슬'; // 창문형
+};
+
+export type TopLoading = {
+  name: '이경호' | '박지민' | '서지수'; // 통돌이
+};
+
+export type FrontLoading = {
+  name: '김나영' | '정우성' | '배수지'; // 드럼
+};
+
+export type BuiltIn = {
+  name: '송혜교' | '유아인' | '이하늘'; // 빌트인
+};
+
+export type Dryer = {
+  name: '박해진' | '정해인' | '김소연'; // 건조기
+};

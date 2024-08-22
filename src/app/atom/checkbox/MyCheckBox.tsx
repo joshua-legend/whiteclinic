2633653675a9ce7381bfed0e3b69fb44d9d3c5ba
misnawk <kms13011@naver.com>
2024-08-22 12:45:09 +0900

    0822 강민석 - 체크박스 컴포넌트 keyof typeof 사용해서 sizes 가져오기

--- src/app/atom/checkbox/MyCheckBox.tsx
+++ src/app/atom/checkbox/MyCheckBox.tsx
@@ -10,8 +10,8 @@ import { Engineer, Input, Order, Revenue, Salary } from './checkbox';
 
 type CheckBoxProps = {
   label: total | '';
-  labelSize?: 'small' | 'medium' | 'large' | 'xlarge'; // 라벨의 크기 지정
-  checkBoxSize?: 'small' | 'medium' | 'large' | 'xlarge'; // 체크박스 크기 지정
+  labelSize?: keyof typeof sizes.fontSize; // 라벨의 크기 지정
+  checkBoxSize?: keyof typeof sizes.fontSize; // 체크박스 크기 지정
 };
 
 const MyCheckBox = ({
@@ -19,18 +19,13 @@ const MyCheckBox = ({
   labelSize = 'medium',
   checkBoxSize = 'xlarge',
 }: CheckBoxProps) => {
-  const fontSize = sizes.fontSize[labelSize];
-  const boxSize = sizes.fontSize[checkBoxSize];
-
   return (
     <FormGroup>
       <FormControlLabel
         control={
-          <Checkbox
-            sx={{ boxSize, '& .MuiSvgIcon-root': { fontSize: sizes.fontSize[checkBoxSize] } }}
-          />
+          <Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: sizes.fontSize[checkBoxSize] } }} />
         }
-        label={<Typography sx={{ fontSize }}>{label}</Typography>}
+        label={<Typography sx={{ labelSize }}>{label}</Typography>}
       />
     </FormGroup>
   );

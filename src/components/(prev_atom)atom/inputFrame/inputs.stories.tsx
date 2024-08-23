0a92d4dd5c44d14285b3674bd77c8faedcb461d0
MyDesktop <dugubi1004@gmail.com>
2024-08-23 17:03:02 +0900

    0823 이지훈 - 스토리북 라이브러리 설정완료 . 화이팅

---
+++ src/components/(prev_atom)atom/inputFrame/inputs.stories.tsx
@@ -0,0 +1,14 @@
+// src/components/inputFrame/inputs.tsx
+
+import InputComponent from './inputs';
+
+const meta = {
+  title: 'components/inputs',
+  component: InputComponent,
+  argTypes: {},
+};
+
+export default meta;
+export const Primary = {
+  args: {},
+};

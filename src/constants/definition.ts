abe850f183d3407e59836dec5645526b0c89b3f9
misnawk <kms13011@naver.com>
2024-08-30 15:39:12 +0900

    0830 강민석 - 체크박스 제네릭화시킴, defintion.js 체크박스 프롭스 수정및 타입데이터추가

--- src/constants/definition.ts
+++ src/constants/definition.ts
@@ -1,4 +1,3 @@
-import { allType } from '@/components/(current_atom)atom/checkbox/()minsuk_checkbox/KMS_CCheckboxData';
 import { sizes } from '@/styles/sizes';
 import { SelectChangeEvent } from '@mui/material';
 import dayjs, { Dayjs } from 'dayjs';
@@ -18,8 +17,31 @@ export type ButtonProps = {
   handleClick?: () => void;
 };
 
-export type CheckboxProps = {
-  label?: allType;
+export type state = '발행완료' | '지급완료' | '휴무추가';
+
+export type revenue = '종합세척' | '일반세척' | '할인적용';
+
+export type engineerName = string;
+
+export type skill =
+  | '벽걸이'
+  | '원웨이'
+  | '포웨이'
+  | '원형'
+  | '스탠드'
+  | '실외기'
+  | '덕트'
+  | '창문형'
+  | '통돌이'
+  | '드럼'
+  | '빌트인'
+  | '건조기';
+
+export type allType = state | revenue | skill;
+
+export type CheckboxProps<T extends allType | engineerName> = {
+  label: T;
+  engineerName: engineerName;
   isChecked?: boolean;
   handleChange?: () => void;
 };

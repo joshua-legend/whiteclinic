dd40e29322edd954766a7009c36767099b5f0a40
misnawk <kms13011@naver.com>
2024-08-30 15:31:56 +0900

    0820 강민석 - pull받자

---
+++ src/components/molecules/KMS/Sales.stories.tsx
@@ -0,0 +1,12 @@
+import { Sales } from './Sales';
+
+const meta = {
+  title: 'atoms/Sales',
+  component: Sales,
+  argTypes: {},
+};
+
+export default meta;
+export const Primary = {
+  args: {},
+};

c2fae5f82aa3e57d8604618aad1e2c8de107f0b6
misnawk <kms13011@naver.com>
2024-08-30 10:22:29 +0900

    0830 강민석 - 체크박스 제네릭으로 변경

--- src/components/(current_atom)atom/checkbox/()minsuk_checkbox/KMS_CCheckbox.tsx
+++ src/components/(current_atom)atom/checkbox/()minsuk_checkbox/KMS_CCheckbox.tsx
@@ -1,17 +1,17 @@
 import { Checkbox, FormControlLabel } from '@mui/material';
-import { allType } from './KMS_CCheckboxData';
+import { allType, engineerName } from './KMS_CCheckboxData';
 
-type KMS_CCheckboxboxProps = {
-  label: allType;
+export type KMS_CCheckboxboxProps<T extends allType | engineerName> = {
+  label: T;
   isChecked?: boolean;
   handleChange?: () => void;
 };
 
-const KMS_CCheckbox = ({
-  label = '덕트',
+const KMS_CCheckbox = <T extends allType | engineerName>({
+  label,
   handleChange,
   isChecked = true,
-}: KMS_CCheckboxboxProps) => {
+}: KMS_CCheckboxboxProps<T>) => {
   return (
     <FormControlLabel
       control={<Checkbox defaultChecked={isChecked} onChange={handleChange} />}

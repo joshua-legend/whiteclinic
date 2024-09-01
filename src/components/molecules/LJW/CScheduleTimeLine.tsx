17724328257a15d9c78c0be1388acc34c06f4d26
easyone1372 <easyone1372@gmail.com>
2024-09-01 19:33:46 +0900

    0901 이지원 - 파일명 변경

--- src/components/molecules/LJW/CScheduleTimeLine.tsx
+++ src/components/molecules/LJW/CScheduleTimeLine.tsx
@@ -1,33 +1,22 @@
-import { CustomerTimeLineProps, workTimeLine } from '@/constants/definition';
-import CScheduleTimeSlot from './CScheduleTimeSlot';
-import { Box, Typography } from '@mui/material';
+import { theme } from '@/constants/theme';
+import { Box, ThemeProvider } from '@mui/material';
+import CEngineerTitle from './CEngineerTitle';
+import { CustomerInfo } from '@/constants/definition';
+import CScheduleTimeMapping from './CScheduleTimeMapping';
 
-//사용자의 예약 시간과 예약 날짜 정보를 timeslot의 시간대와 기사의 날짜에 대입하여 mapping
-//데이터 확정되면 enginnerInfo 타입을 추가해서 해당 타입과 customer 정보를 비교해서 집어넣는 것으로 수정할 예정
-const CScheduleTimeLine = ({ engineerDate, orderInfo }: CustomerTimeLineProps) => {
+type CScheduleTimeLineProps = {
+  engineerName: string;
+  engineerDate: string;
+  orderInfo?: CustomerInfo[];
+};
+const CScheduleTimeLine = ({ engineerName, engineerDate, orderInfo }: CScheduleTimeLineProps) => {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-      {workTimeLine && workTimeLine.length > 0 ? (
-        workTimeLine.map((timeSlot, index) => {
-          const customer = orderInfo?.find(
-            (order) =>
-              order.appointmentTime === timeSlot.label && order.appointmentDate === engineerDate
-          );
-
-          return (
-            <CScheduleTimeSlot
-              key={index}
-              timeSlot={timeSlot.label || 'default time'}
-              customer={customer}
-            />
-          );
-        })
-      ) : (
-        <Typography variant="body1" sx={{ color: '#ffffff' }}>
-          정보 없음.
-        </Typography>
-      )}
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '6px' }}>
+        <CEngineerTitle engineerName={engineerName} />
+        <CScheduleTimeMapping engineerDate={engineerDate} orderInfo={orderInfo} />
+      </Box>
+    </ThemeProvider>
   );
 };
 

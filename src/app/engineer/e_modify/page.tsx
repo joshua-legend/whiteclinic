3c6b093308846cf00b65addb5818ba7d9b26eb0d
misnawk <kms13011@naver.com>
2024-09-06 14:07:20 +0900

    0906 강민석 - 페이지 링크주소 일치화

---
+++ src/app/engineer/e_modify/page.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function Page() {
+  return <div>기사님 수정페이지</div>;
+}
--- src/components/molecules/KMS/Organism/engineerInfo/RightInfoComponent.tsx
+++ src/components/molecules/KMS/Organism/engineerInfo/RightInfoComponent.tsx
@@ -1,24 +1,39 @@
 import CCheckbox from '@/components/atom/CCheckbox';
 import CDropDown from '@/components/atom/CDropdown';
-import { Box } from '@mui/material';
+import { Box, SelectChangeEvent } from '@mui/material';
 import { state, WeekDays } from '@/constants/definition';
 import CDatePicker from '@/components/atom/CDatePicker';
-
-const rightinfo = [
-  [
-    '정기휴무',
-    <CDropDown key="weekdays" contentList={WeekDays} contentName="요일 선택" />,
-    <CCheckbox<state> key="dayoffCheckbox" label="휴무추가" isChecked={false} />,
-  ],
-
-  [
-    '비정기휴무',
-    <CDatePicker key="calenderComponent" />,
-    <CCheckbox<state> key="vacationCheckbox" label="휴무추가" isChecked={false} />,
-  ],
-];
+import { useState } from 'react';
 
 export const RightInfoComponent = () => {
+  const [regularHoliday, setRegularHoliday] = useState<string>();
+  const [irregularHoliday, setIrregularHoliday] = useState();
+
+  const handleRegularHoliday = (event:SelectChangeEvent<string>) => {
+    setRegularHoliday(event.target.value);
+    console.log(regularHoliday);
+  };
+
+  const rightinfo = [
+    [
+      '정기휴무',
+      <CDropDown
+        key="weekdays"
+        contentList={WeekDays}
+        contentName="요일 선택"
+        handleChange={handleRegularHoliday}
+        selectedValue={regularHoliday}
+      />,
+      <CCheckbox<state> key="dayoffCheckbox" label="휴무추가" isChecked={false} />,
+    ],
+
+    [
+      '비정기휴무',
+      <CDatePicker key="calenderComponent" />,
+      <CCheckbox<state> key="vacationCheckbox" label="휴무추가" isChecked={false} />,
+    ],
+  ];
+
   return (
     <Box
       sx={{

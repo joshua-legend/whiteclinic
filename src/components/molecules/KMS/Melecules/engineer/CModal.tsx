215464540f16aa3b75bb4397c9e1cb0c2954ac24
misnawk <kms13011@naver.com>
2024-09-06 20:38:26 +0900

    0906 강민석 - C 모달 수정

--- src/components/molecules/KMS/Melecules/engineer/CModal.tsx
+++ src/components/molecules/KMS/Melecules/engineer/CModal.tsx
@@ -4,6 +4,21 @@ import { ButtoModalTextType, modalProps } from '@/constants/definition';
 export const CModal = ({ open = false, title, children }: modalProps) => {
   return (
     <Modal
+      sx={{
+        position: 'absolute' as 'absolute',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 415,
+        height: 70,
+        border: '1px solid #000',
+        boxShadow: 24,
+        backgroundColor: 'white',
+        p: 4,
+      }}
       open={open}
       aria-labelledby="parent-modal-title"
       aria-describedby="parent-modal-description"

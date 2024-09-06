125e660af99611b1f3bc44d3d8fc5724721ce8e6
misnawk <kms13011@naver.com>
2024-09-06 21:04:52 +0900

    0906 강민석 - CModal 수정중

--- src/components/molecules/KMS/Melecules/engineer/CModal.tsx
+++ src/components/molecules/KMS/Melecules/engineer/CModal.tsx
@@ -1,33 +1,28 @@
 import { Box, Modal, Typography } from '@mui/material';
 import { ButtoModalTextType, modalProps } from '@/constants/definition';
 
+const CModalStyle = {
+  position: 'absolute' as 'absolute',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: 10,
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 415,
+  height: 70,
+  border: '1px solid #000',
+  boxShadow: 24,
+  backgroundColor: 'whitesmoke',
+  p: 4,
+};
+
 export const CModal = ({ open = false, title, children }: modalProps) => {
   return (
-    <Modal
-      sx={{
-        position: 'absolute' as 'absolute',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 415,
-        height: 70,
-        border: '1px solid #000',
-        boxShadow: 24,
-        backgroundColor: 'whitesmoke',
-        p: 4,
-      }}
-      open={open}
-      aria-labelledby="parent-modal-title"
-      aria-describedby="parent-modal-description"
-    >
-      <Box sx={{ ...CModal }}>
-        <Typography variant="h6" component="h2">
-          {title}
-        </Typography>
-        {children}
+    <Modal open={open}>
+      <Box sx={{ ...CModalStyle }}>
+        <Typography variant="h6">{title}</Typography>
+        <Box>{children}</Box>
       </Box>
     </Modal>
   );

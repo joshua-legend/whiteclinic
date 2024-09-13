c9aef0b10edd2054c222552b759416fda9622b8c
misnawk <kms13011@naver.com>
2024-09-13 20:49:34 +0900

    0913 강민석 - Salary 테이블 분리 완료

--- src/components/molecules/KMS/Organism/engineer/Engineer.tsx
+++ src/components/molecules/KMS/Organism/engineer/Engineer.tsx
@@ -175,26 +175,6 @@ export const Engineer = () => {
       })
     ),
 
-    // engineerCreateData(
-    //   '정기휴무',
-    //   CInput({
-    //     labelProp: '정기휴무',
-    //     type: 'text',
-    //     placeholderProp: '정기휴무가 있을 시 기입하세요.',
-    //     handleInput: (e) => EngineerInfoChangeHandler('regularDay', e.target.value),
-    //     value: engineerData.regularDay,
-    //   })
-    // ),
-    // engineerCreateData(
-    //   '비정기휴무',
-    //   CInput({
-    //     labelProp: '비정기휴무',
-    //     type: 'text',
-    //     placeholderProp: '비정기휴무가 있을 시 기입하세요.',
-    //     handleInput: (e) => EngineerInfoChangeHandler('irregular', e.target.value),
-    //     value: engineerData.irregular,
-    //   })
-    // ),
   ];
 
   return (
